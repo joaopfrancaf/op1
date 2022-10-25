d48199afefbe7d1c2b4c34278f2cfe26acb57ad4
João Pedro França Freitas <joaopfrancaf@gmail.com>
2022-10-24 21:31:14 -0300

    first commit

---
+++ src/pages/create.tsx
@@ -0,0 +1,13 @@
+import { Head } from "next/document";
+import Header from "../components/header";
+import Input from "../components/input";
+
+export default function create () {
+    return (
+      <>
+      <Header/>
+      <Input/>
+      </>
+      
+    )
+}
